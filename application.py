399926f3b0586163955c9d6862a2814b0e1ad0b5
Thiago Nascimento Nogueira <tnnogueira@gmail.com>
2020-10-27 06:50:08 -0300

    application simples

--- application.py
+++ application.py
@@ -5,30 +5,28 @@ import pickle as pkl
 
 app = Flask(__name__)
 
-os.chdir(os.path.dirname(__file__))
-with open('model/decision_tree.pkl', 'rb') as fp:
-    model = pkl.load(fp)
+# os.chdir(os.path.dirname(__file__))
+# with open('model/decision_tree.pkl', 'rb') as fp:
+#     model = pkl.load(fp)
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    request_json = request.get_json()
-    predict_list = [
-        request_json["sepal_length_cm"],
-        request_json["sepal_width_cm"],
-        request_json["petal_length_cm"],
-        request_json["petal_width_cm"]
-        ]
+# @app.route('/predict', methods=['POST'])
+# def predict():
+#     request_json = request.get_json()
+#     predict_list = [
+#         request_json["sepal_length_cm"],
+#         request_json["sepal_width_cm"],
+#         request_json["petal_length_cm"],
+#         request_json["petal_width_cm"]
+#         ]
 
-    prediction = model.predict([predict_list])[0]
-    return make_response(prediction, 200)
+#     prediction = model.predict([predict_list])[0]
+#     return make_response(prediction, 200)
 
 
-@app.route('/')
-@app.route('/available')
-@app.route('/health')
-@app.route('/ready')
-def ok():
-    return 200
+@app.route("/")
+def hello():
+    return "Hello, World!"
 
-if __name__ == '__main__':
-    app.run()
+@app.route("/test")
+def hello_test():
+    return test.ret_message()
